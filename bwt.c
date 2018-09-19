273a61c27411cda1e51996a56cabee3ee65596e8
TheodoreDev <theodore.huynh.melb@gmail.com>
2018-09-19 19:06:42 +1000

    Add intial file

---
+++ bwt.c
@@ -0,0 +1,35 @@
+/*
+COMP9319 Web Data Compression and Search
+Assignment 2: BWT and Search 
+
+Name: Nguyen Minh Thong Huynh
+ID: z5170141
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
