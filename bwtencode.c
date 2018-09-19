98d7255e5667d127539bb4a986b023f7c74f4cea
TheodoreDev <theodore.huynh.melb@gmail.com>
2018-09-19 20:22:59 +1000

    Add makefile and complete read file into memory

--- bwtencode.c
+++ bwtencode.c
@@ -6,6 +6,11 @@ Name: Nguyen Minh Thong Huynh
 ID: z5170141
 */
 
+/* 
+Example command:
+./bwtencode "|" ./tmp ./dummy.txt ./dummy.bwt
+*/
+
 /* libraries */
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,6 +27,18 @@ ID: z5170141
 
 
 /* functions */
+char* read_file(char *input_path){
+    FILE *f = fopen(input_path, "rb");
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);  //same as rewind(f);
+
+    char *string = malloc(fsize + 1);
+    fread(string, fsize, 1, f);
+    fclose(f);
+    string[fsize] = 0;
+    return string;
+}
 
 /* Main Program */
 int main(int argc, char **argv){
@@ -34,14 +51,27 @@ int main(int argc, char **argv){
     char* index_path = (char*) argv[2];
     char* input_path = (char*) argv[3];
     char* output_path = (char*) argv[4];
-    
+
     #ifdef DEBUG
         printf("Delimiter: %c\n", *delimiter);
         printf("Index Path: %s\n", index_path);
         printf("Input Path: %s\n", input_path);
-        printf("Output Path: %s\n", output_path);
+        printf("Output Path: %s\n\n", output_path);
+    #endif
+    
+    #ifdef DEBUG
+        printf("Reading file!\n");
+    #endif
+    char* input_s;
+    input_s = read_file(input_path);
+    #ifdef DEBUG 
+        printf("%s\n\n", input_s); 
+    #endif
+
+    #ifdef DEBUG
         printf("Starting to encode!\n");
     #endif
+    
 
 }
 
---
+++ makefile
@@ -0,0 +1,5 @@
+bwtencode: bwtencode.c
+	gcc -Wall bwtencode.c -o bwtencode
+
+clean:
+	rm bwtencode
