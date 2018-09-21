b1a98b4ead15a4598957b1120252be735c550fb2
TheodoreDev <theodore.huynh.melb@gmail.com>
2018-09-22 00:04:37 +1000

    Complete BWT with sorted deliminators sorting

--- bwtencode.c
+++ bwtencode.c
@@ -19,6 +19,7 @@ Example command:
 
 /* global variables */
 char* text_str;
+char* delimiter;
 
 /* constants */
 #define DEBUG
@@ -54,19 +55,28 @@ short get_string_frequency(char* string, int string_len, int freq[]){
     return unique_chars;
 }
 
-// int strcmp_custom(const char* s1, const char* s2)
-// {
-//     while(*s1 && (*s1==*s2) && (s1!))
-//         s1++,s2++;
-//     return *(const unsigned char*)s1-*(const unsigned char*)s2;
-// }
+int strcmp_custom(const char* s1, const char* s2){
+    while(*s1 && (*s1==*s2)){
+        if ((*s1==*delimiter)&&(*s2==*delimiter)){
+            if(s1 > s2){
+                return 1;
+            } else if (s1 < s2){
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+        s1++, s2++;
+    }
+    return *(const unsigned char*)s1-*(const unsigned char*)s2;
+}
 
 int suffix_str_compare_func(const void *indexp_1, const void *indexp_2){
     int index_1 = *(int*) indexp_1;
     int index_2 = *(int*) indexp_2;
     char* str_1 = &text_str[index_1];
     char* str_2 = &text_str[index_2];
-    return strcmp(str_1, str_2);
+    return strcmp_custom(str_1, str_2);
 }
 
 /* Main Program */
@@ -76,7 +86,7 @@ int main(int argc, char **argv){
 		fprintf(stderr, "ERROR: Missing inputs\n");
     }
 
-    char* delimiter = (char*) argv[1];
+    delimiter = (char*) argv[1];
     char* index_path = (char*) argv[2];
     char* input_path = (char*) argv[3];
     char* output_path = (char*) argv[4];
@@ -122,6 +132,22 @@ int main(int argc, char **argv){
         }
     #endif
 
+    #ifdef DEBUG
+        printf("Delimiter positions:\n");
+    #endif
+    int* del_pos = (int*) malloc(freq[(int)(*delimiter)]*sizeof(int));
+    int del_index = 0;
+    for (int i = 0; i < text_len; i++){
+        if (text_str[i] == *delimiter){
+            del_pos[del_index++]=i;
+        };
+    }
+    #ifdef DEBUG
+    for (int i = 0; i < del_index; i++){
+        printf("%d ", del_pos[i]);
+    }
+    #endif
+
     /* initialize suffix array */
     #ifdef DEBUG
         printf("\ninitializing suffix array:\n");
@@ -129,36 +155,52 @@ int main(int argc, char **argv){
 
     FILE *output_file = fopen(output_path, "wb");
     int index;
-    
+    int cur_row=0;
+
     for (int i = 0; i < ALPHABETS_NUM; i++){    
         if (freq[i] > 0){
         
             suffix_indexes = (int*) malloc(freq[i]*sizeof(int));
             index = 0;
-            for (int j = 0; j < ALPHABETS_NUM; j ++){
+            for (int j = 0; j < text_len; j ++){
                 if ( (int)text_str[j]== i){
                     suffix_indexes[index++]=j;
                 }
             }
             qsort(suffix_indexes, index, sizeof(int), suffix_str_compare_func);
-            printf("%c:", (char)i);
+            #ifdef DEBUG
+                printf("%c:", (char)i);
             for (int k = 0; k < index; k ++){
                 printf(" %d|", suffix_indexes[k]);
             }
             printf("\n");
+            #endif
 
             char bwt_char;
+            int bwt_pos;
             for (int k = 0; k < index; k ++){
-                printf("-%d", suffix_indexes[k]-1);
+                // #ifdef DEBUG
+                //     printf("-%d", suffix_indexes[k]-1);
+                // #endif
                 if (suffix_indexes[k] != 0){
-                    bwt_char = text_str[suffix_indexes[k]-1];
+                    bwt_pos = suffix_indexes[k]-1;
+                    bwt_char = text_str[bwt_pos];
                 } else{
-                    bwt_char = text_str[text_len-1];
+                    bwt_pos = text_len-1;
+                    bwt_char = text_str[bwt_pos];
                 }
-                printf("->%c\n", bwt_char);
-                /*fprintf(output_file, "%c",bwt_char);*/
-                /*fflush(stdout);*/
-                /*fputc(bwt_char, output_file);*/
+                if (bwt_char == *delimiter){
+                    for (int t=0; t<del_index; t++){
+                        if (del_pos[t]==bwt_pos){
+                            printf("Del (%d) is at row %d for pos %d\n",t+1, cur_row, del_pos[t]);
+                        }
+                    }
+                }
+                cur_row++;
+                // #ifdef DEBUG
+                //     printf(" -> %c\n", bwt_char);
+                // #endif
+                fputc(bwt_char, output_file);
             }
 
             free (suffix_indexes);
